41781e1fd703a4278db61cc21261bbdfaf2b24e5
julimontif <julimontif@gmail.com>
2024-07-30 12:23:38 -0400

    Intro a Router

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route, Link } from "react-router-dom"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Inicio</Link>
+          </li>
+          <li>
+            <Link to="/portafolio">Portafolio</Link>
+          </li>
+        </ul>
+      </nav>
+      <section>
+        <Routes>
+          <Route path="/" element={<h1>Inicio</h1>}></Route>
+          <Route path="/portafolio" element={<h1>Portafolio</h1>}></Route>
+        </Routes>
+      </section>
     </div>
   );
 }
